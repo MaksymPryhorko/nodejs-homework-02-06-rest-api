33324ca242dfca993ae0f5752a5420dddf2f3478
MaksymPryhorko <78167917+MaksymPryhorko@users.noreply.github.com>
2021-12-28 20:22:49 +0200

    Сделал необязательное задание по дз-4.
    Сделать фильтрацию контактов по полю избранного (GET /contacts?favorite=true)

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -4,12 +4,13 @@ const { Contact } = require("../../models");
 
 const listContacts = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const contacts = await Contact.find({ owner: _id }, "", { skip, limit: Number(limit) }).populate(
-    "owner",
-    "_id name email"
-  );
+  const contactFindOptions = favorite ? { owner: _id, favorite } : { owner: _id };
+  const contacts = await Contact.find(contactFindOptions, "", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "_id name email");
   res.json({
     message: "success",
     code: 200,
